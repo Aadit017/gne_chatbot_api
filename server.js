10c25881c85b74eb9f361c220dbb6a21c742bff2
Aadit <baggaaadit017@gmail.com>
2023-05-06 12:16:34 +0530

    index html file added

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const bodyParser=require('body-parser')
 const PORT=3000
 const app=express()
 
+app.use(bodyParser.json())
+let data=[]
+app.get('/data', (req, res) => {
+    res.json(data);
+  });
 
 app.get('/', (req, res) => {
-    res.send('working')
+    res.sendFile('index.html', {root: __dirname })
+    // res.send(`<h1>PORT ${PORT} </h1>`)
   })
-  
+
 app.listen(PORT,()=>{
     console.log(`The server is running at the port ${PORT}`);
 })
